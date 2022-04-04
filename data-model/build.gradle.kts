b48b52de6dcc419e0abdd50907dee90f1a416b49
kotharironak <53209990+kotharironak@users.noreply.github.com>
2022-04-04 09:48:19 +0530

    chore: update gradle version and fixed snyk failure (#37)
    * chore: update gradle version and fixed snyk failure
    
    * update constraints jackson-core
    
    * fixed implementation to api
    
    * update codeowners
    
    * addressed comments of updgrading to 7.4.2 and others
    
    * upgraded to latest

--- data-model/build.gradle.kts
+++ data-model/build.gradle.kts
@@ -16,6 +16,13 @@ dependencies {
     api("org.apache.commons:commons-compress:1.21") {
       because("Multiple vulnerabilities in avro-declared version")
     }
+    api("com.fasterxml.jackson.core:jackson-databind:2.13.2.2") {
+      because(
+        "Denial of Service (DoS) " +
+          "[High Severity][https://snyk.io/vuln/SNYK-JAVA-COMFASTERXMLJACKSONCORE-2421244] in " +
+          "com.fasterxml.jackson.core:jackson-databind@2.13.1"
+      )
+    }
   }
   api("commons-codec:commons-codec:1.14")
   api("io.micrometer:micrometer-core:1.5.3")
