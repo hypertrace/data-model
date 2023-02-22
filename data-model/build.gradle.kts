b7bb3e704f8379b78bd272882a3814e569ca67a5
kotharironak <53209990+kotharironak@users.noreply.github.com>
2023-02-22 16:56:45 +0530

    fix: updating libs for vulnerabilities (#43)

--- data-model/build.gradle.kts
+++ data-model/build.gradle.kts
@@ -16,12 +16,8 @@ dependencies {
     api("org.apache.commons:commons-compress:1.21") {
       because("Multiple vulnerabilities in avro-declared version")
     }
-    api("com.fasterxml.jackson.core:jackson-databind:2.13.2.2") {
-      because(
-        "Denial of Service (DoS) " +
-          "[High Severity][https://snyk.io/vuln/SNYK-JAVA-COMFASTERXMLJACKSONCORE-2421244] in " +
-          "com.fasterxml.jackson.core:jackson-databind@2.13.1"
-      )
+    api("com.fasterxml.jackson.core:jackson-databind:2.14.2") {
+      because("version 2.12.7.1 has a vulnerability https://snyk.io/vuln/SNYK-JAVA-COMFASTERXMLJACKSONCORE-3038424")
     }
   }
   api("commons-codec:commons-codec:1.14")
