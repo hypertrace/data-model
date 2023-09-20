522f78c49ffbd0b72e82f1a7d31f2ae2ad4222b7
Shivansh Anand Srivastava <shivansh.srivastava@traceable.ai>
2023-09-20 15:45:54 +0530

    Expose resource string attribute from SpanAttributeUtils (#44)
    * Expose resource string attribute from SpanAttributeUtils
    
    * Remove javax nullable
    
    * Remove javax nullable

--- data-model/src/test/java/org/hypertrace/core/datamodel/shared/SpanAttributeUtilsTest.java
+++ data-model/src/test/java/org/hypertrace/core/datamodel/shared/SpanAttributeUtilsTest.java
@@ -2,14 +2,20 @@ package org.hypertrace.core.datamodel.shared;
 
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import org.hypertrace.core.datamodel.AttributeValue;
 import org.hypertrace.core.datamodel.Attributes;
 import org.hypertrace.core.datamodel.Event;
+import org.hypertrace.core.datamodel.Resource;
+import org.hypertrace.core.datamodel.StructuredTrace;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -62,5 +68,28 @@ class SpanAttributeUtilsTest {
                 .build());
     final List<String> result = SpanAttributeUtils.getStringListAttribute(event, "attributeKey");
     assertEquals(List.of("value1", "value2", "3"), result);
+  }
+
+  @Test
+  void testGetResourceStringAttribute() {
+    final Event event = mock(Event.class);
+    final StructuredTrace trace = mock(StructuredTrace.class);
+    Resource resource =
+        Resource.newBuilder()
+            .setAttributes(
+                Attributes.newBuilder()
+                    .setAttributeMap(
+                        Map.of("key1", AttributeValue.newBuilder().setValue("value1").build()))
+                    .build())
+            .build();
+    when(event.getResourceIndex()).thenReturn(0);
+    when(trace.getResourceList()).thenReturn(Collections.singletonList(resource));
+    Optional<String> valueAbsent =
+        SpanAttributeUtils.getResourceStringAttribute(trace, event, "key2");
+    Optional<String> valuePresent =
+        SpanAttributeUtils.getResourceStringAttribute(trace, event, "key1");
+    assertFalse(valueAbsent.isPresent());
+    assertTrue(valuePresent.isPresent());
+    assertEquals("value1", valuePresent.get());
   }
 }
